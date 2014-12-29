63b00936494a987288eb142a4aa83bc3bf2929d3
lythesia <iranaikimi@gmail.com>
2014-12-29 19:50:26 +0800

    title blink

--- source/js/gallery.js
+++ source/js/gallery.js
@@ -1,4 +1,13 @@
 (function($){
+  // Blink
+  var timer = setInterval(function() {
+    var title = $(document).attr('title'),
+       $title = $('#header a:first');
+    var new_title = title.indexOf("_") < 0 ? ">> _" : ">>  ";
+    document.title = new_title;
+    $title.html(new_title);
+  }, 500);
+
   // Caption
   $('.entry').each(function(i){
     $(this).find('img').each(function(){
